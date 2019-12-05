4cd1d0461619dce76559cba6c5ae70bd66541f4d
Bradley Haley <bshaley25@gmail.com>
2019-12-05 15:54:43 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -39,3 +39,8 @@ def aoh_update(aoh, row, key, new_value)
   # Return the updated AoH
   aoh[row][key] = new_value
 end
+
+
+aoh_update(assembled_aoh, 3, :occupation, "Prostitute")
+
+puts assembled_aoh
